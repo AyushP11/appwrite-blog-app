71d96e565b371f292f8888f7d56629c77dc3f284
AyushP11 <ayushpanchakshari@gmail.com>
2024-01-07 16:34:34 +0530

    changes done

---
+++ src/components/index.js
@@ -0,0 +1,32 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from './container/Container'
+import LogoutBtn from "./Header/LogoutBtn";
+import Logo from "./Logo";
+import Button from "./Button";
+import Input from "./Input";
+import Login from "./Login";
+import RTE from "./RTE";
+import PostForm from './post-form/PostForm'
+import AuthLayout from './AuthLayout'
+import PostCard from './PostCard'
+import Select from './Select'
+import Signup from './Signup'
+
+
+export {
+  Header, 
+  Footer, 
+  Container, 
+  LogoutBtn, 
+  Logo, 
+  Button, 
+  Input, 
+  Login, 
+  RTE,
+  PostForm,
+  AuthLayout,
+  PostCard,
+  Select,
+  Signup
+}
